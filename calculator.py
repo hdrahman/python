b8e6068e7882191d5017b28b61bf83007503c11e
hdrahman <haamed1.450@gmail.com>
2020-03-24 20:47:26 +0300

    simple calculator

---
+++ calculator.py
@@ -0,0 +1,17 @@
+x = input ('enter addition, subtraction, multiplication or division')
+num1 = input('enter first number:')
+num2 = input('enter second number:')
+resultNum = 0
+
+if x == 'addition':
+  resultNum = int(num1) + int(num2)
+if x == 'subtraction':
+  resultNum = int(num1) - int(num2)
+if x == 'division':
+  resultNum = int(num1) / int(num2)
+if x == 'multiplication':
+  resultNum = int(num1) * int(num2)
+
+print('the ',x,' of ',num1,' and ',num2,' is {3}', resultNum )
+
+
