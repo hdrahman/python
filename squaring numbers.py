c951baef996a392fb55bfb9dac723efc9341e1b6
hdrahman <62461412+hdrahman@users.noreply.github.com>
2020-03-26 21:45:05 +0300

    Add files via upload

---
+++ squaring numbers.py
@@ -0,0 +1,3 @@
+num1 = input ('enter first number')
+num_sqrt = int(num1) ** 2
+print(num1,'squared is equal to',num_sqrt)
---
+++ subtraction.py
@@ -0,0 +1,4 @@
+num1 = input('enter first number:')
+num2 = input('enter second number:')
+difference = int(num1) - int(num2)
+print('The difference between {0} and {1} is {2}'. format(num1, num2, difference))  
