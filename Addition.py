59f6566281c34db28cfd07c94ffc68e64dd3b3c2
Abdul Rahman <you@example.com>
2020-06-27 19:42:54 +0300

    update to addition.py

--- Addition.py
+++ Addition.py
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
+
 num1 = input('enter first number: ')
 num2 = input('enter second number: ')
 sum = int(num1) + int(num2)
-=======
-num1 = input('enter first number: ')
-num2 = input('enter second number: ')
-sum = int(num1) + int(num2)
->>>>>>> c951baef996a392fb55bfb9dac723efc9341e1b6
+
+print("updated on june 27")
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))  
